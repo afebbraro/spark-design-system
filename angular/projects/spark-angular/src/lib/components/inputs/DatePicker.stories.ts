d8c21717f8aaae97934ab6d12bb2c4be9440d51a
corcorankt1 <guern1kn@gmail.com>
2020-11-11 12:10:36 -0500

    update stories

--- angular/projects/spark-angular/src/lib/components/inputs/DatePicker.stories.ts
+++ angular/projects/spark-angular/src/lib/components/inputs/DatePicker.stories.ts
@@ -9,6 +9,7 @@ import { SprkFieldErrorModule } from '../../directives/inputs/sprk-field-error/s
 import { SprkDatepickerModule } from '../../directives/inputs/sprk-datepicker/sprk-datepicker.module';
 import { SparkInputContainerModule } from './sprk-input-container/sprk-input-container.module';
 import { SparkInputContainerComponent } from './sprk-input-container/sprk-input-container.component';
+import { SprkIconInputContainerModule } from './sprk-icon-input-container/sprk-icon-input-container.module';
 import { SprkDatepickerDirective } from '../../directives/inputs/sprk-datepicker/sprk-datepicker.directive';
 import { markdownDocumentationLinkBuilder } from '../../../../../../../storybook-utilities/markdownDocumentationLinkBuilder';
 
@@ -62,6 +63,7 @@ const modules = {
     SprkIconModule,
     SprkFieldErrorModule,
     SprkDatepickerModule,
+    SprkIconInputContainerModule,
   ],
 };
 
